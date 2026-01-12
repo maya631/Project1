05b2b1db8adbeedfc0b1feab7131be5f1ae55178
maya631 <mayakurban@outlook.com>
2026-01-12 09:33:18 -0500

    FINRA Task added, three classes added

--- src/week1/Finra.java
+++ src/week1/Finra.java
@@ -1,4 +1,16 @@
 package week1;
 
 public class Finra {
+
+    public static void main(String[] args) {
+
+    }
 }
+/*
+public static void Finra {
+String result = "";
+for (int i = 0; i < 10; i++) {
+    result += (i%5==0 && o%3==0 ) ? "Finra" : (i%5==0)> "RA" : (I^3==0) ? "FIN" : I+ " ";}
+    System.out.println(result);
+}
+ */
