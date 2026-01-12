419ec2935ecfa5ee5976345d500572229127772a
maya631 <mayakurban@outlook.com>
2026-01-12 10:36:31 -0500

    week1 solution

---
+++ src/maya/DivisionWithoutOperator.java
@@ -0,0 +1,16 @@
+package maya;
+
+public class DivisionWithoutOperator {
+    int a = 10;
+    int b = 2;
+    int c = a/b;
+
+
+
+    public static void main(String[] args) {
+        //write a method that can divide two numbers without using division operator
+        DivisionWithoutOperator divisionWithoutOperator = new DivisionWithoutOperator();
+        System.out.println(divisionWithoutOperator.c);
+
+    }
+}
---
+++ src/maya/OddOrEven.java
@@ -0,0 +1,4 @@
+package maya;
+
+public class OddOrEven {
+}
